d6a2571c8ab7a4690a866607e127841c03539dd7
emman6578 <motaemman6578@gmail.com>
2025-03-15 15:04:24 +0800

    R4

--- src/libraries/API/Auth/config.tsx
+++ src/libraries/API/Auth/config.tsx
@@ -1,5 +1,2 @@
-export const API_URL = "http://192.168.1.42:3007/api";
-// export const API_URL = "https://infant-guard-backend.onrender.com/api";
-
-//TODO: admin side
-//Download data by excel - when downloaded all infants sort it by baranggay
+// export const API_URL = "http://192.168.1.42:3007/api";
+export const API_URL = "https://infant-guard-backend.onrender.com/api";
